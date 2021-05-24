b6d8cda3214a81e2e0d000c636b1e481169e6d73
Uchencho <aloziekelechi17@gmail.com>
2021-05-24 22:22:29 +0100

    send token functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-termii
 
 go 1.15
+
+require github.com/pkg/errors v0.9.1
---
+++ token.go
@@ -0,0 +1,46 @@
+package gotermii
+
+import (
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+// SendTokenRequest is a representation of a send token request
+type SendTokenRequest struct {
+	APIKey         string `json:"api_key"`
+	MessageType    string `json:"message_type"`
+	To             string `json:"to"`
+	From           string `json:"from"`
+	Channel        string `json:"channel"`
+	PinAttempts    int    `json:"pin_attempts"`
+	PinTimeToLive  int    `json:"pin_time_to_live"`
+	PinLength      int    `json:"pin_length"`
+	PinPlaceholder string `json:"pin_placeholder"`
+	MessageText    string `json:"message_text"`
+	PinType        string `json:"pin_type"`
+}
+
+// SendTokenResponse is a representation of a send token response
+type SendTokenResponse struct {
+	PinID     string `json:"pinId"`
+	To        string `json:"to"`
+	SmsStatus string `json:"smsStatus"`
+}
+
+// SendTokenFunc provides the functionality of sending a token request
+type SendTokenFunc func(req SendTokenRequest) (SendTokenResponse, error)
+
+// SendToken sends a token request
+func (c Client) SendToken() SendTokenFunc {
+	return func(req SendTokenRequest) (SendTokenResponse, error) {
+		req.APIKey = c.config.APIKey
+		rURL := "api/sms/otp/send"
+
+		var tokenResponse SendTokenResponse
+		if err := c.makeRequest(http.MethodPost, rURL, req, tokenResponse); err != nil {
+			return SendTokenResponse{}, errors.Wrap(err, "error in making request to send otp token")
+		}
+		return tokenResponse, nil
+	}
+}
