b89efd06f2a0219e07cae77c8013ce1e62fa530c
Uchencho <aloziekelechi17@gmail.com>
2021-05-25 20:41:11 +0100

    verify token workflow

--- token.go
+++ token.go
@@ -21,6 +21,20 @@ type SendTokenRequest struct {
 	PinType        string `json:"pin_type"`
 }
 
+// VerifyTokenRequest is a representation of a verify token request
+type VerifyTokenRequest struct {
+	APIKey string `json:"api_key"`
+	PinID  string `json:"pin_id"`
+	Pin    string `json:"pin"`
+}
+
+// VerifyTokenResponse is a representation of a verify token response
+type VerifyTokenResponse struct {
+	PinID    string `json:"pinId"`
+	Verified string `json:"verified"`
+	Msisdn   string `json:"msisdn"`
+}
+
 // SendTokenResponse is a representation of a send token response
 type SendTokenResponse struct {
 	PinID     string `json:"pinId"`
@@ -36,6 +50,18 @@ func (c Client) SendToken(req SendTokenRequest) (SendTokenResponse, error) {
 	var tokenResponse SendTokenResponse
 	if err := c.makeRequest(http.MethodPost, rURL, req, &tokenResponse); err != nil {
 		return SendTokenResponse{}, errors.Wrap(err, "error in making request to send otp token")
+	}
+	return tokenResponse, nil
+}
+
+// VerifyToken sends a request to verify token
+func (c Client) VerifyToken(req VerifyTokenRequest) (VerifyTokenResponse, error) {
+	req.APIKey = c.config.APIKey
+	rURL := "api/sms/otp/verify"
+
+	var tokenResponse VerifyTokenResponse
+	if err := c.makeRequest(http.MethodPost, rURL, req, &tokenResponse); err != nil {
+		return VerifyTokenResponse{}, errors.Wrap(err, "error in making request to verify otp token")
 	}
 	return tokenResponse, nil
 }
