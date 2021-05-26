4c3e56de92532ee8ebe0fc0efec3d29100f45c1f
Uchencho <aloziekelechi17@gmail.com>
2021-05-26 20:33:48 +0100

    fixed tests

--- termii_test.go
+++ termii_test.go
@@ -16,6 +16,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const (
+	termiiTestApiKey = "test-API"
+)
+
 func fileToStruct(filepath string, s interface{}) io.Reader {
 	bb, _ := ioutil.ReadFile(filepath)
 	json.Unmarshal(bb, s)
@@ -23,7 +27,7 @@ func fileToStruct(filepath string, s interface{}) io.Reader {
 }
 
 func TestSendTokenSuccess(t *testing.T) {
-	os.Setenv("TERMII_API_KEY", "test-API")
+	os.Setenv("TERMII_API_KEY", termiiTestApiKey)
 	var (
 		expectedTokenRequest termii.SendTokenRequest
 		receivedBody         termii.SendTokenRequest
@@ -67,7 +71,7 @@ func TestSendTokenSuccess(t *testing.T) {
 }
 
 func TestVerifyTokenSuccess(t *testing.T) {
-	os.Setenv("TERMII_API_KEY", "test-API")
+	os.Setenv("TERMII_API_KEY", termiiTestApiKey)
 	var (
 		expectedTokenRequest termii.VerifyTokenRequest
 		receivedBody         termii.VerifyTokenRequest
@@ -106,6 +110,50 @@ func TestVerifyTokenSuccess(t *testing.T) {
 
 	t.Run("Response is as expected", func(t *testing.T) {
 		fileToStruct(filepath.Join("testdata", "verify_token_response.json"), &expectedResponse)
+		assert.Equal(t, expectedResponse, resp)
+	})
+}
+
+func TestGetInAppTokenSuccess(t *testing.T) {
+	os.Setenv("TERMII_API_KEY", termiiTestApiKey)
+	var (
+		expectedTokenRequest termii.GenerateTokenRequest
+		receivedBody         termii.GenerateTokenRequest
+		req                  termii.GenerateTokenRequest
+		expectedResponse     termii.GenerateTokenResponse
+	)
+
+	termiiService := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if err := json.NewDecoder(req.Body).Decode(&receivedBody); err != nil {
+			log.Printf("error in unmarshalling %+v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		t.Run("Request is as expected", func(t *testing.T) {
+			fileToStruct(filepath.Join("testdata", "generate_token_request.json"), &expectedTokenRequest)
+			assert.Equal(t, expectedTokenRequest, receivedBody)
+		})
+
+		var resp termii.GenerateTokenResponse
+		fileToStruct(filepath.Join("testdata", "generate_token_response.json"), &resp)
+
+		w.WriteHeader(http.StatusOK)
+		bb, _ := json.Marshal(resp)
+		w.Write(bb)
+	}))
+	os.Setenv("TERMII_URL", termiiService.URL)
+	fileToStruct(filepath.Join("testdata", "generate_token_request.json"), &req)
+
+	c := termii.NewClient()
+
+	resp, err := c.GetInAppToken(req)
+	t.Run("No error is returned", func(t *testing.T) {
+		assert.NoError(t, err)
+	})
+
+	t.Run("Response is as expected", func(t *testing.T) {
+		fileToStruct(filepath.Join("testdata", "generate_token_response.json"), &expectedResponse)
 		assert.Equal(t, expectedResponse, resp)
 	})
 }
