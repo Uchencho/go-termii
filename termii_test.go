93b8653e4b01e18dd89a05fb0ba6b18a9998cdce
Uchencho <aloziekelechi17@gmail.com>
2021-05-28 10:52:37 +0100

    cleaned up tests

--- termii_test.go
+++ termii_test.go
@@ -212,3 +212,53 @@ func TestFetchSenderIDSuccess(t *testing.T) {
 		assert.Equal(t, expectedResponse, resp)
 	})
 }
+
+func TestRegisterSenderSuccess(t *testing.T) {
+	os.Setenv("TERMII_API_KEY", termiiTestApiKey)
+	var (
+		expectedTokenRequest termii.RegisterSenderIdRequest
+		receivedBody         termii.RegisterSenderIdRequest
+		req                  termii.RegisterSenderIdRequest
+		expectedResponse     termii.RegisterSenderResponse
+	)
+
+	termiiService := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if err := json.NewDecoder(req.Body).Decode(&receivedBody); err != nil {
+			log.Printf("error in unmarshalling %+v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		t.Run("URL and request method is as expected", func(t *testing.T) {
+			expectedURL := "/api/sender-id/request"
+			assert.Equal(t, http.MethodPost, req.Method)
+			assert.Equal(t, expectedURL, req.RequestURI)
+		})
+
+		t.Run("Request is as expected", func(t *testing.T) {
+			fileToStruct(filepath.Join("testdata", "register_sender_request.json"), &expectedTokenRequest)
+			assert.Equal(t, expectedTokenRequest, receivedBody)
+		})
+
+		var resp termii.RegisterSenderResponse
+		fileToStruct(filepath.Join("testdata", "register_sender_response.json"), &resp)
+
+		w.WriteHeader(http.StatusOK)
+		bb, _ := json.Marshal(resp)
+		w.Write(bb)
+	}))
+	os.Setenv("TERMII_URL", termiiService.URL)
+	fileToStruct(filepath.Join("testdata", "register_sender_request.json"), &req)
+
+	c := termii.NewClient()
+
+	resp, err := c.RegisterSender(req)
+	t.Run("No error is returned", func(t *testing.T) {
+		assert.NoError(t, err)
+	})
+
+	t.Run("Response is as expected", func(t *testing.T) {
+		fileToStruct(filepath.Join("testdata", "register_sender_response.json"), &expectedResponse)
+		assert.Equal(t, expectedResponse, resp)
+	})
+}
